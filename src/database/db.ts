c3ea0de3b5d8a71ba4324454b7ed38d4ebceb6bf
TSantiagoIdalgo <130645404+TSantiagoIdalgo@users.noreply.github.com>
2023-12-21 15:12:56 -0300

    ps

--- src/database/db.ts
+++ src/database/db.ts
@@ -6,6 +6,14 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false,
+  host: DB_HOST,
+  dialect: "postgres",
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
 });
 
 export default sequelize;
