da5ec36e1fa4965771de03364d36c299c6248fef
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2024-01-04 19:07:25 -0300

    SSL/TSL

--- src/services/mail/mail.services.ts
+++ src/services/mail/mail.services.ts
@@ -6,8 +6,8 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   host: process.env.HOST,
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD
