f50e2de48e36906ad8cf3ed33696f620f3b4c822
CSRuggeri <santiruu@gmail.com>
2024-01-07 14:07:24 -0300

    origin accsess

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4000;
 
 async function Server (typeDefs: DocumentNode[], resolvers: any) {
   const corsOptions = {
-    origin: 'https://p-final-p-ccorp-front.vercel.app'
+    origin: ['https://p-final-p-ccorp-front.vercel.app', 'http://localhost:5173'],
   };
   const apolloServer = new ApolloServer({ typeDefs, resolvers });
   const server = express();
