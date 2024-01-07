21500b7e1778057fb170befeb90727e03760938f
CSRuggeri <santiruu@gmail.com>
2024-01-07 13:56:06 -0300

    allow origin

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4000;
 
 async function Server (typeDefs: DocumentNode[], resolvers: any) {
   const corsOptions = {
-    origin: 'http://localhost:5173'
+    origin: '*'
   };
   const apolloServer = new ApolloServer({ typeDefs, resolvers });
   const server = express();
