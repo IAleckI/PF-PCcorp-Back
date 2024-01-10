2eb82375a2e65c816b4b9af1a6076311a1861e2d
IAleckI <healsava12@gmail.com>
2024-01-10 09:23:49 -0500

    update template

--- src/services/mail/template.ts
+++ src/services/mail/template.ts
@@ -14,7 +14,7 @@ export function mailTemplate (name: string,token: string) {
         <h1 style="color: #000;">Hi 👋 <span style="color: #48e;">${name}</span></h1>
         <p style="font-size: 20px; color: #000; text-align: center;">It's great that you consider using our services, we offer the best products, at the lowest price so you can fulfill the dream of your gaming PC.</p>
         <p style="font-size: 20px; color: #000;">Please click on the link below to verify your email address:</p>
-        <a style="text-decoration: none; color: #000; border: 2px solid #48e;; border-radius: 10px; padding: 15px; background: transparent; cursor: pointer; font-size: 15px;" href="http://localhost:5173/verify/?token=${token}">verify yourself now</a>
+        <a style="text-decoration: none; color: #000; border: 2px solid #48e;; border-radius: 10px; padding: 15px; background: transparent; cursor: pointer; font-size: 15px;" href="https://p-final-p-ccorp-front.vercel.app/verify/?token=${token}">verify yourself now</a>
     </main>
   </body>
   </html>
