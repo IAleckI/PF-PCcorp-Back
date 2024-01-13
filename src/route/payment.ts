dd666f0a459aba29ac416d22032a2b4157cf84e1
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2024-01-13 13:43:14 -0300

    v4

--- src/route/payment.ts
+++ src/route/payment.ts
@@ -9,6 +9,7 @@ paymentRoute.get("/", (_req: Request, res: Response) => {
 });
 
 paymentRoute.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
   res.status(200).json(req.body);
 });
 
