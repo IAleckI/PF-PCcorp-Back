5a7432ddea2efc29138788f02cdcc10081422591
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2024-01-13 12:49:34 -0300

    webhook

--- src/route/upload.ts
+++ src/route/upload.ts
@@ -1,15 +1,20 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response } from "express";
 
-import ProductController from '../controllers/productController';
+import ProductController from "../controllers/productController";
 const uploadRouter = Router();
 
-uploadRouter.post('/', async (req: Request, res: Response) => {
+uploadRouter.post("/", async (req: Request, res: Response) => {
   try {
     const createdProduct = await ProductController.createProduct(req, res);
     res.json(createdProduct);
   } catch (error:any) {
     res.status(500).json({ error: error.message });
   }
+});
+
+uploadRouter.post("/webhook", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(200).json(req.body);
 });
 
 export default uploadRouter;
