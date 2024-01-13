eefec1dc704bd81c3305e9768b955b0eeca14763
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2024-01-13 13:17:28 -0300

    v3

--- src/route/payment.ts
+++ src/route/payment.ts
@@ -3,6 +3,12 @@ import { Request, Response } from "express";
 
 const paymentRoute = Router();
 
+
+paymentRoute.get("/", (_req: Request, res: Response) => {
+
+  res.status(200).send("ok");
+});
+
 paymentRoute.post("/", (req: Request, res: Response) => {
   console.log(req.body);
   res.status(200).send("ok");
